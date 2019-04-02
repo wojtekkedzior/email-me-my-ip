cd5023f5dc92b909f1489769e99b8fd5bd3009c2
Kedzior, Wojtek <wowaked@gmail.com>
2019-04-02 14:42:32 +0200

    adding db access

--- src/main/java/getMyIP/UrlLookupService.java
+++ src/main/java/getMyIP/UrlLookupService.java
@@ -3,38 +3,42 @@ package getMyIP;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import getMyIP.model.URLHealthCheck;
+import getMyIP.repository.URLCheckRepository;
 
 @Service
 public class UrlLookupService {
 	private final Logger log = LoggerFactory.getLogger(Runner.class);
 	
+	@Autowired
+	private URLCheckRepository urlCheckRepo;
+	
     @Async
     public CompletableFuture<String> findUser(String urlToCheck) throws InterruptedException {
-    	// log.info("Looking up " + user);//
-    //    String url = String.format("https://api.github.com/users/%s", user);
-        //User results = restTemplate.getForObject(url, User.class);
-    	
     	String state = new String();
+    	URLHealthCheck findByUrl = urlCheckRepo.findByUrl(urlToCheck);
+    	Date now = new Date(System.currentTimeMillis());
     	
 		try {
 			log.info("Checking URL: " + urlToCheck);
-//			URLHealthCheck findByUrl = urlCheckRepo.findByUrl(listedUrl);
 			
-//			if (findByUrl == null) {
-//				findByUrl = new URLHealthCheck();
-//				findByUrl.setUrl(listedUrl);
-//			}
+			if (findByUrl == null) {
+				findByUrl = new URLHealthCheck();
+				findByUrl.setUrl(urlToCheck);
+			}
 			
-//			Date now = new Date(System.currentTimeMillis());
-//			findByUrl.setLastChecked(now);
+			findByUrl.setLastChecked(now);
 			
 			URL url = new URL(urlToCheck);
 			HttpURLConnection.setFollowRedirects(true);
@@ -44,28 +48,28 @@ public class UrlLookupService {
 
 			state = http.getResponseCode() == 200 ? "up" : "down";
 			
-//			findByUrl.setState(state);
+			findByUrl.setState(state);
 
 			if(http.getResponseCode() != 200) {
-//				findByUrl.setFailures(findByUrl.getFailures() +1);
-//				findByUrl.setLastFailure(now);
+				findByUrl.setFailures(findByUrl.getFailures() +1);
+				findByUrl.setLastFailure(now);
 				
 				log.warn("Got state: " + state + " for URL: " + urlToCheck);
 			}
 			
-//			findByUrl.setCount(findByUrl.getCount() + 1);
+			findByUrl.setCount(findByUrl.getCount() + 1);
 
-//			urlCheckRepo.save(findByUrl);
+			urlCheckRepo.save(findByUrl);
 
 		} catch (IOException e) {
 //			String state = "down";
-//			findByUrl.setState(state);
-//
-//			findByUrl.setFailures(findByUrl.getFailures() +1);
-//			findByUrl.setLastFailure(now);
-//			findByUrl.setCount(findByUrl.getCount() + 1);
-//
-//			urlCheckRepo.save(findByUrl);
+			findByUrl.setState(state);
+
+			findByUrl.setFailures(findByUrl.getFailures() +1);
+			findByUrl.setLastFailure(now);
+			findByUrl.setCount(findByUrl.getCount() + 1);
+
+			urlCheckRepo.save(findByUrl);
 			
 			log.error(urlToCheck + " " + e.toString());
 			return CompletableFuture.completedFuture("down");
